f17e91378b1c749f34e4e38c73392678307d2e72
Danielacarranza <60374156+Danielacarranza@users.noreply.github.com>
2020-06-04 22:47:57 -0500

    Add files via upload

---
+++ Practica_7.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main() {
+	int num,n,i,mult;
+	printf("Ingresar su numero de base:\n");
+	scanf("%d",&num);
+	printf("Ingrese los multiplos a generar:\n");
+	scanf("%d",&n);
+	for(i=1;i<=n;i++){
+		mult=num*i;
+		printf("%d\n",mult);
+	}
+	return 0;
+}
